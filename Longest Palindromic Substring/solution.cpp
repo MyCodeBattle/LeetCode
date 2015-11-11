20d4bb80e58458aa59cb6c0c64d36d6429813d1e
PureFrog <mycodebattle@outlook.com>
2015-11-11 23:48:34 +0800

    卡了O(n^2)真是没意思

---
+++ Longest Palindromic Substring/solution.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string str, ans;
+    int **mp;
+
+    int dfs(int l, int r) {
+        if (l > r) {
+            return true;
+        }
+        if (mp[l][r] != -1) return mp[l][r];
+        mp[l][r] = 0;
+        if (str[l] == str[r] && dfs(l+1, r-1)) {
+            mp[l][r] = 1;
+            if (r-l+1 > ans.size()) {
+                ans = str.substr(l, r-l+1);
+            }
+        }
+        dfs(l+1, r);
+        dfs(l, r-1);
+        return mp[l][r];
+    }
+
+    string longestPalindrome(string _s) {
+        str = _s;
+        if (_s.empty()) {
+            return "";
+        }
+        int n = _s.size();
+        mp = new int*[n];
+        for (int i = 0; i < n; i++) {
+            mp[i] = new int[n];
+        }
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) mp[i][j] = -1;
+        dfs(0, _s.size()-1);
+        return ans;
+    }
+};
