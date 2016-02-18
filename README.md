a7413f1bd72a74bff39b1064986b9f17acc7a293
PureFrog <mycodebattle@outlook.com>
2016-02-18 18:04:07 +0800

    303

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 | 情况 |  题号  |  标题  |  语言  |
 |------|-----|---|---|
 |√| **316**| **Remove Duplicate Letters** | **C++**|
+|√| 302 | Range Sum Query - Immutable | Java |
 |√| 301 | Remove Invalid Parentheses | Java |
 |√| 300 | Longest Increasing Subsequence | Java |
 |√| 299 | Bulls and Cows | Java |
---
+++ Range Sum Query - Immutable/solution.java
@@ -0,0 +1,21 @@
+public class NumArray {
+    int[] sum;
+
+    public NumArray(int[] nums) {
+        sum = new int[nums.length];
+        for (int i = 0; i < nums.length; ++i)
+            sum[i] = i == 0 ? nums[i] : nums[i] + sum[i - 1];
+    }
+
+    public int sumRange(int i, int j) {
+        int l = Math.max(0, i);
+        int r = Math.min(sum.length - 1, j);
+        return l == 0 ? sum[r] : sum[r] - sum[l - 1];
+    }
+}
+
+
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray = new NumArray(nums);
+// numArray.sumRange(0, 1);
+// numArray.sumRange(1, 2);
